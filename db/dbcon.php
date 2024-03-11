9ee5a635229d32d48b73d0b4a5aed87ba750c171
Vipe2249 <153647424+Vipe2249@users.noreply.github.com>
2024-03-11 11:06:15 +0200

    Update dbcon.php

--- db/dbcon.php
+++ db/dbcon.php
@@ -1,16 +1,15 @@
 <?php
-// Database configuration
+
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'infinityware');
 
-// Attempt to establish a connection to the database
+
 $con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-// Check the connection
+
 if (!$con) {
-    // Handle connection error gracefully
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
