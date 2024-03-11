9bfc1bc2493e6be9ac44bad9fb758006dd48cddc
Vipe2249 <153647424+Vipe2249@users.noreply.github.com>
2024-03-11 11:13:19 +0200

    Update notify.php

--- php/notify.php
+++ php/notify.php
@@ -1,19 +1,19 @@
 <?php
-// Tell Payfast that this page is reachable by triggering a header 200
+
 http_response_code(200);
 
 define('SANDBOX_MODE', true);
 $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
 
-// Posted variables from ITN
+
 $pfData = $_POST;
 
-// Sanitize posted variables
+
 foreach ($pfData as $key => $val) {
     $pfData[$key] = htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
 }
 
-// Convert posted variables to a string
+
 $pfParamString = '';
 foreach ($pfData as $key => $val) {
     if ($key !== 'signature') {
@@ -26,7 +26,7 @@ foreach ($pfData as $key => $val) {
 $pfParamString = rtrim($pfParamString, '&');
 
 function pfValidSignature($pfData, $pfParamString, $pfPassphrase = null) {
-    // Calculate security signature
+
     if ($pfPassphrase === null) {
         $tempParamString = $pfParamString;
     } else {
@@ -38,10 +38,10 @@ function pfValidSignature($pfData, $pfParamString, $pfPassphrase = null) {
 }
 
 function pfValidIP() {
-    // List of valid IP addresses
+
     $validIps = [
-        '154.0.161.0',  // Example IP address
-        '154.0.161.1'   // Example IP address
+        '154.0.161.0',  
+        '154.0.161.1'   
     ];
 
     // Get referrer IP
@@ -55,15 +55,15 @@ function pfValidPaymentData($cartTotal, $pfData) {
 }
 
 function pfValidServerConfirmation($pfParamString, $pfHost = 'sandbox.payfast.co.za') {
-    // Use cURL (if available)
+
     if (function_exists('curl_version')) {
-        // Variable initialization
+
         $url = 'https://' . $pfHost . '/eng/query/validate';
 
-        // Create cURL object
+
         $ch = curl_init();
 
-        // Set cURL options
+
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $pfParamString);
@@ -71,7 +71,7 @@ function pfValidServerConfirmation($pfParamString, $pfHost = 'sandbox.payfast.co
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 
-        // Execute cURL
+
         $response = curl_exec($ch);
         curl_close($ch);
 
@@ -88,8 +88,8 @@ $check3 = pfValidPaymentData("1105", $pfData);
 $check4 = pfValidServerConfirmation($pfParamString, $pfHost);
 
 if ($check1 && $check2 && $check3 && $check4) {
-    // All checks have passed, the payment is successful
+
 } else {
-    // Some checks have failed, check payment manually and log for investigation
+
 }
 ?>
