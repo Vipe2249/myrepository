9d2e932a08c7d099b0d0bdd7f5bf8e9dfda0b8ca
Vipe2249 <153647424+Vipe2249@users.noreply.github.com>
2024-03-11 11:13:53 +0200

    Update payfast.php

--- php/payfast.php
+++ php/payfast.php
@@ -12,22 +12,22 @@
  */
 
  
-// Assuming $jsVariable is available from process.php
-$jsVariable = isset($jsVariable) ? $jsVariable : 0; // Default value if $jsVariable is not set
 
-// Update the data array with $jsVariable
+$jsVariable = isset($jsVariable) ? $jsVariable : 0; 
+
+
 $data['custom_int1'] = $jsVariable;
 
 
 function generateSignature($data, $passPhrase = null) {
-    // Create parameter string
+
     $pfOutput = '';
     foreach( $data as $key => $val ) {
         if($val !== '') {
             $pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
         }
     }
-    // Remove last ampersand
+
     $getString = substr( $pfOutput, 0, -1 );
     if( $passPhrase !== null ) {
         $getString .= '&passphrase='. urlencode( trim( $passPhrase ) );
@@ -35,23 +35,23 @@ function generateSignature($data, $passPhrase = null) {
     return md5( $getString );
 } 
 
-// here
+
 
 $cartTotal = $total;
 $passphrase = 'jt7NOE43FZPn';
 $data = array(
-    // Merchant details
+
     'merchant_id' => '10000100',
     'merchant_key' => '46f0cd694581a',
     'return_url' => 'http://localhost/infinityware/php/index.php',
     'cancel_url' => 'http://www.yourdomain.co.za/cancel.php',
     'notify_url' => 'http://localhost/infinityware/php/notify.php',
-    // Buyer details
+
     'name_first' => 'First Name',
     'name_last'  => 'Last Name',
     'email_address'=> 'megawluke@gmail.com',
-    // Transaction details
-    'm_payment_id' => '1234', //Unique payment ID to pass through to notify_url
+
+    'm_payment_id' => '1234', 
     'amount' => number_format( sprintf( '%.2f', $cartTotal ), 2, '.', '' ),
     'item_name' => 'Order#123',
     'payment_method' => 'cc'
@@ -60,7 +60,7 @@ $data = array(
 $signature = generateSignature($data, $passphrase);
 $data['signature'] = $signature;
 $htmlForm = "";
-// If in testing mode make use of either sandbox.payfast.co.za or www.payfast.co.za
+
 $testingMode = true;
 $pfHost = $testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
 foreach($data as $name=> $value)
