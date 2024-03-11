abf9a175273d6b74cf7a42367eb7705add7512fb
Vipe2249 <153647424+Vipe2249@users.noreply.github.com>
2024-03-11 11:00:26 +0200

    Create footer.php

---
+++ header/footer.php
@@ -0,0 +1,47 @@
+  
+<footer>
+    <div class="container">
+        <div class="footer-top">
+            <div class="footer-columns-6">
+               
+            </div>
+        </div>
+        <div class="footer-middle">
+            <div class="footer-columns-3">
+                <div class="footer-item" id="first">
+                    <strong>Information</strong>
+                    <a href="terms-and-conditions">Terms and Conditions</a>
+                    <a href="returns-and-refunds">Returns and Refunds</a>
+                    <a href="Payment Info">Payment Info</a>
+                    <a href="Delivery Info">Delivery Info</a>
+                </div>
+                <div class="footer-item" id="middle">
+                    <strong>About Us</strong>
+                    <p>We are a business that opened in January 2024, so although we are new, it would be our pleasure to serve you!</p>
+                </div>
+                <div class="footer-item" id="last">
+                    <strong>Contact Us</strong>
+                        <p>083 725 1683<i class="fa-solid fa-phone footer-icon"></i></p>
+                        <p>sales@infinityware.co.za<i class="fa-solid fa-envelope footer-icon"></i></p>
+                        <div class="contact">
+                            <div class="contact-icon">
+                            <i class="fa-solid fa-shop footer-icon"></i>
+                            </div>
+                            <div class="contact-address">
+                        <p>Sandpebbles Estate, Kipling House, Chelsea Road</p>
+                        <p>Theescombe</p>
+                        <p>Gqeberha</p>
+                        <p>6070</p>
+                        </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2024 JL Computer Technologies (Pty) Ltd. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+
+</footer>
+    
